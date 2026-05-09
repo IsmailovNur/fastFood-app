4da700d9270595c49ca90b1e54c1e1d904e70443
Nur Ismailov <nismailovwork@gmail.com>
2026-05-09 11:09:55 +0600

    initialize project, prepaired files & folders to development

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="main-title">
+          FastFood order App
+        </h1>
+        <div className="content">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
